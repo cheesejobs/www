d830c9cb8c5765c18c90fbb8747a9a0ae8c64e70
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-10-30 19:41:36 +0100

    Refactor

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,54 +2,53 @@ const Promise = require('bluebird')
 const path = require('path')
 const jobTemplate = path.resolve('./src/templates/job.js')
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
+exports.createPages = async ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
 
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
-          {
-            allJobsYaml {
-              edges {
-                node {
-                  path
+  const graphqlPromise = () =>
+    new Promise((resolve, reject) => {
+      resolve(
+        graphql(
+          `
+            {
+              allJobsYaml {
+                edges {
+                  node {
+                    path
+                  }
                 }
               }
             }
-          }
-        `
-      ).then(result => {
-        if (result.errors) {
+          `
+        ).then(result => {
+          if (!result.errors) return result
           console.log(result.errors)
-          reject(result.errors)
-        }
+          return reject(result.errors)
+        })
+      )
+    })
 
-        const jobs = result.data.allJobsYaml.edges.map(item => item.node)
-
-        jobs.forEach(job =>
-          createPage({
-            path: job.path,
-            component: jobTemplate,
-            context: {
-              path: job.path,
-              companyId: job.companyId
-            }
-          })
-        )
-      })
-    )
-  })
+  const result = await graphqlPromise()
+  const jobs = result.data.allJobsYaml.edges.map(item => item.node)
+  jobs.forEach(job =>
+    createPage({
+      path: job.path,
+      component: jobTemplate,
+      context: {
+        path: job.path,
+        companyId: job.companyId
+      }
+    })
+  )
 }
 
-exports.modifyWebpackConfig = ({ config, stage }) => {
-  return config.merge({
+exports.modifyWebpackConfig = ({ config, stage }) =>
+  config.merge({
     resolve: {
       alias: {
-        Components: __dirname + '/src/components',
-        Containers: __dirname + '/src/containers',
-        Helpers: __dirname + '/src/helpers'
+        Components: path.join(__dirname, '/src/components'),
+        Containers: path.join(__dirname, '/src/containers'),
+        Helpers: path.join(__dirname, '/src/helpers')
       }
     }
   })
-}
